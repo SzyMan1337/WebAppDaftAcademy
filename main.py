5f977a788da12963ee0a45df4b2cdf33fdb5e56f
SzyMan1337 <szymanski.mateusz@gmail.com>
2021-04-18 09:24:55 +0200

    Changes in main.py

--- main.py
+++ main.py
@@ -7,5 +7,21 @@ def root():
     return {"message": "Hello world!"}
 
 @app.get("/method")
-def method():
+def method_get():
     return {"method": "GET"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.delete("/method")
+def method_del():
+    return {"method": "DELETE"}
+
+@app.options("/method")
+def method_opt():
+    return {"method": "OPTIONS"}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
