12eba039036fa49d2d9ad2582cb666a0bb68e362
SzyMan1337 <szymanski.mateusz@gmail.com>
2021-04-18 10:13:19 +0200

    Changes in main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from datetime import date
+from datetime import timedelta
 
 app = FastAPI()
+i = 0
 
 @app.get("/")
 def root():
@@ -25,3 +28,15 @@ def method_opt():
 @app.post("/method", status_code=201)
 def method_post():
     return {"method": "POST"}
+
+
+
+
+@app.post("/register/{name}/{surname}",status_code=201)
+def registerPost():
+    i = i +1
+    dlugos = len(name)+len(surname)
+    today = date.today
+    day2 = today - timedelta(days=dlugos); 
+    return {"id": 1, "name": f"{name}", "register_date": f"{today.strftime("%Y-%m-%d")}", "vaccination_date": f"{day2.strftime("%Y-%m-%d")}"}
+
